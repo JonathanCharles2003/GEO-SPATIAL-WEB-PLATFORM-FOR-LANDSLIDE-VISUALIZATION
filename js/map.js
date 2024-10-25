39328796bafe7a98cf88a0756c48d23ca7e9e4f6
JonathanCharles2003 <jb2366@srmist.edu.in>
2024-10-25 11:56:41 +0530

    geojson in js folder

--- js/map.js
+++ js/map.js
@@ -74,7 +74,7 @@ function initializeMap() {
         });
 
     // Load GeoJSON layer but don't add to map immediately
-    fetch("../data/foothills.geojson")
+    fetch("/js/foothills.geojson")
         .then((response) => response.json())
         .then((geojsonData) => {
             geojsonLayer = L.geoJSON(geojsonData, {
